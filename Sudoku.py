fdf8debab6b0295f4e535f9f6447c64229cad26b
ssglob <edwardlaw21@hotmail.com>
2024-01-10 09:17:07 -0800

    started sudoku app

--- Sudoku.py
+++ Sudoku.py
@@ -1,5 +1,5 @@
 from kivy.app import App
-from kivy.uix.stacklayout import StackLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.image import Image
 from kivy.uix.button import Button
@@ -8,13 +8,13 @@ from sudoku_solver import Solution
 
 class Sudoku(App):
     def build(self):
-        self.window = StackLayout()
-        self.start_game = Button(
-            text = "Generate game",
-            color = '#ffffff'
-        )
+        self.window = BoxLayout(spacing = 20,orientation = "vertical")
+        self.sudoku_image = Image(source='sudoku_image.jpg',size_hint = (1,.3),pos_hint = {'top':1})
+        self.window.add_widget(self.sudoku_image)
+        self.start_game = Button(text = "Generate game",font_size = 30,size_hint = (1,.3), pos_hint = {'center_y':1})
         self.start_game.bind(on_press = self.game)
         self.window.add_widget(self.start_game)
+        return self.window
     def game(self,event):
         pass
 
